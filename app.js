f3a27856b739ca7092850a7f83fa330cb26dcbba
Patrick Murphy <user@users-mbp-2.home>
2018-02-24 14:28:17 -0600

    adding comments

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 
 
-//Fire up express
+//Fire up express and basic setup
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
-var login = require('./controllers/login.js')
+//Route vars
+var login = require('./controllers/login.js');
 
-app.use('/login', login)
+//Routes use
+app.use('/login', login);
 
+//Starting server
 app.listen(3000, function() {
   console.log("Listening on port 3000");
 });
