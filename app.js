3fc4678b07a605c19b750bff332faa3f5a7be2dd
Patrick Murphy <user@users-mbp-2.home>
2018-02-24 00:37:38 -0600

    Adding code

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
+var login = require('controllers/login.js')
+
+app.use('/login', login)
+
 app.listen(3000, function() {
   console.log("Listening on port 3000");
 });
